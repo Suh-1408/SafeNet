45aa56c249ee5f0b9493e7bf70811d121ec71254
Suh-1408 <120710478+Suh-1408@users.noreply.github.com>
2025-04-13 18:00:56 +0530

    commit for render deployment

--- code/main.py
+++ code/main.py
@@ -109,4 +109,6 @@ def predict_url():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port
+    app.run(host='0.0.0.0', port=port)
+
